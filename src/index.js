39acd6b5b6316cdfc33be2ad53991a38aa078257
Sai Nikhil <95617782+sainikhil1601@users.noreply.github.com>
2022-09-30 18:57:19 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+app = express()
+app.use(express.static('reactFolderName/build'));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'reactFolderName', 'build', 'index.html')));
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
